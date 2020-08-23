f8745804cfe223cbb37500430022a186d819714a
陈凯悦 <44251986+Carterwade1997@users.noreply.github.com>
2020-08-23 18:28:05 +0800

    Iinitial the leetcode workapace

---
+++ 1.two-sum.cpp
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode id=1 lang=cpp
+ *
+ * [1] Two Sum
+ *
+ * https://leetcode.com/problems/two-sum/description/
+ *
+ * algorithms
+ * Easy (45.11%)
+ * Likes:    13729
+ * Dislikes: 504
+ * Total Accepted:    2.6M
+ * Total Submissions: 5.8M
+ * Testcase Example:  '[2,7,11,15]\n9'
+ *
+ * Given an array of integers, return indices of the two numbers such that they
+ * add up to a specific target.
+ * 
+ * You may assume that each input would have exactly one solution, and you may
+ * not use the same element twice.
+ * 
+ * Example:
+ * 
+ * 
+ * Given nums = [2, 7, 11, 15], target = 9,
+ * 
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1].
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+    }
+};
+// @lc code=end
+
